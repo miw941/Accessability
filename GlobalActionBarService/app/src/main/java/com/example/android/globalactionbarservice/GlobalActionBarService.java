cc59534ed50bc0cb2631a17f6d1228ca6c12fe82
miw941 <98720982+miw941@users.noreply.github.com>
2022-01-31 14:05:58 +0000

    Update GlobalActionBarService.java

--- GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
+++ GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
@@ -35,8 +35,22 @@ public class GlobalActionBarService extends AccessibilityService {
     FrameLayout mLayout;
     ...
         
-   @Override
-   protected void onServiceConnected() {       
+    @Override
+    protected void onServiceConnected() {  
+       // Create an overlay and display the action bar
+       WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+       mLayout = new FrameLayout(this);
+       WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+       lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
+       lp.format = PixelFormat.TRANSLUCENT;
+       lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+       lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+       lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+       lp.gravity = Gravity.TOP;
+       LayoutInflater inflater = LayoutInflater.from(this);
+       inflater.inflate(R.layout.action_bar, mLayout);
+       wm.addView(mLayout, lp);
+    }
            
 
     @Override
